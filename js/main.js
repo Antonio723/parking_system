aeeb95e4af2ff76d2ff5e126db832e3522d4def5
AntonioAiltonGoncalves <antonioailton723@gmail.com>
2021-06-01 08:02:56 -0300

    preparo botoes

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const precos = document.getElementById("precos");
+const adicionar = document.getElementById("adiciona");
+
+
+precos.addEventListener('click', exibirTabela);
+
+function exibirTabela() {
+    console.log('click');
+    window.location.href = "precos.html";
+}
+
+
+adicionar.addEventListener('click', registrar);
+
+function registrar() {
+    const infos = {
+        placa: document.getElementById('placa').value,
+        nome: document.getElementById('nome').value
+    };
+    const infos = [];
+
+    info = { placa, nome }
+
+    infos.push(info)
+    localStorage.setItem("infos", JSON.stringify(infos));
+
+    // var storedNames = JSON.parse(localStorage.getItem("infos"));
+
+
+    console.log("alo", info);
+}
